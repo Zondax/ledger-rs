efc42e32fc9e6e02b6953ecabb22b0271d5d2d23
Juan Leni <lenijuan@gmail.com>
2018-07-30 17:27:58 +0200

    basic hidapi test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #[cfg(test)]
-mod tests {
+mod hidapi_checks {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn retrieve_devices() {
+        extern crate hidapi;
+
+        let api = hidapi::HidApi::new().unwrap();
+        for device in &api.devices() {
+            println!("{:#?}", device);
+        }
     }
 }
