5d37bdaf0458c4a1d191473b7aa5a680d6cebf92
rllola <rllola80@gmail.com>
2020-06-13 18:08:13 +0200

    Using demo app from zemu;

--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,23 @@
 const ledger = require('./ledger-node')
 const Zemu = require('@zondax/zemu').default
+const path = require('path')
 
-// Error: elfPath cannot be null!
-// Need a default app to use here...
-const sim = new Zemu();
+//Zemu.checkAndPullImage();
+
+process.on("SIGINT", () => {
+  Zemu.stopAllEmuContainers(function () {
+    process.exit();
+  });
+});
+
+const sim = new Zemu(path.join(__dirname,'/node_modules/@zondax/zemu/bin/demoApp/app.elf'));
 const APP_SEED = "equip will roof matter pink blind book anxiety banner elbow sun young";
 
 const sim_options = {
     logging: true,
     custom: `-s "${APP_SEED}"`,
     press_delay: 150
-    //,X11: true
+    , X11: true
 };
 
 sim.start(sim_options)
