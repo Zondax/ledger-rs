7c2d02bb1d8dce6aca389efe547c1568c1813da9
rllola <rllola80@gmail.com>
2020-06-15 16:55:15 +0200

    Wasm test using demoApp from zemu;

--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,57 @@
-const ledger = require('./ledger-node')
-const Zemu = require('@zondax/zemu').default
-const path = require('path')
+const ledger = require('./ledger-node');
+const Zemu = require('@zondax/zemu').default;
+const path = require('path');
+const assert = require('assert');
 
-//Zemu.checkAndPullImage();
-
-process.on("SIGINT", () => {
-  Zemu.stopAllEmuContainers(function () {
-    process.exit();
+const catchExit = async () => {
+  process.on("SIGINT", () => {
+    Zemu.stopAllEmuContainers(function () {
+      process.exit();
+    });
   });
-});
-
-const sim = new Zemu(path.join(__dirname,'/node_modules/@zondax/zemu/bin/demoApp/app.elf'));
-const APP_SEED = "equip will roof matter pink blind book anxiety banner elbow sun young";
-
-const sim_options = {
-    logging: true,
-    custom: `-s "${APP_SEED}"`,
-    press_delay: 150
-    , X11: true
 };
 
-sim.start(sim_options)
-  .then(async () => {
-    var transport = sim.getTransport();
+describe("LEDGER TEST", function () {
+  this.timeout(10000);
+
+  var sim,
+      transport;
+
+  before(async function() {
+    // runs before tests start
+    await catchExit();
+    await Zemu.checkAndPullImage();
+    await Zemu.stopAllEmuContainers();
+
+    sim = new Zemu(path.join(__dirname,'/node_modules/@zondax/zemu/bin/demoApp/app.elf'));
+    const APP_SEED = "equip will roof matter pink blind book anxiety banner elbow sun young";
+    const sim_options = {
+        logging: true,
+        custom: `-s "${APP_SEED}"`,
+        press_delay: 150
+        ,X11: true
+    };
+
+    await sim.start(sim_options);
+
+    transport = sim.getTransport();
+  });
+
+  after(async function() {
+    // runs after all the test are done
     await sim.close();
+    // reset
+    transport = null;
   })
+
+  it("#deviceInfo()", async function() {
+    const resp = await ledger.deviceInfo(transport);
+
+    console.log(resp);
+
+    assert("targetId" in resp);
+    assert("seVersion" in resp);
+    assert("flag" in resp);
+    assert("mcuVersion" in resp);
+  });
+})
