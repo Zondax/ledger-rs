27cb6bcdf914f44effbfab40b793c670dfbb7705
microengineer <microengineer18@gmail.com>
2021-01-20 07:06:29 +0100

    cargo fmt

--- ledger-transport-tcp/src/lib.rs
+++ ledger-transport-tcp/src/lib.rs
@@ -2,9 +2,8 @@ mod errors;
 
 use ledger_apdu::{APDUAnswer, APDUCommand};
 
-use std::net::{TcpStream};
 use std::io::{Read, Write};
-
+use std::net::TcpStream;
 
 pub use errors::LedgerTCPError;
 
@@ -12,13 +11,12 @@ pub struct TransportTCP {
     url: String,
 }
 
-
 impl TransportTCP {
     pub fn new(host: &str, port: u16) -> Self {
         Self {
             url: format!("{}:{}", host, port),
-        } 
-    } 
+        }
+    }
 
     pub async fn exchange(&self, command: &APDUCommand) -> Result<APDUAnswer, LedgerTCPError> {
         let raw_command = command.serialize();
@@ -27,21 +25,24 @@ impl TransportTCP {
 
         log::debug!("successfully connected to server {}", &self.url);
 
-        fn wrap_err(raw_command: &Vec<u8>, stream: &mut TcpStream) -> Result<APDUAnswer, std::io::Error> {
+        fn wrap_err(
+            raw_command: &Vec<u8>,
+            stream: &mut TcpStream,
+        ) -> Result<APDUAnswer, std::io::Error> {
             // store length as 32bit big endian into array
             let send_length_bytes = (raw_command.len() as u32).to_be_bytes();
-            
+
             // first send number of bytes
             stream.write(&send_length_bytes[..])?;
-            
+
             // then send bytes
             stream.write(&raw_command[..])?;
 
-            let mut rcv_length_bytes = [0u8;4];
-            
-            // first read number of bytes 
+            let mut rcv_length_bytes = [0u8; 4];
+
+            // first read number of bytes
             stream.read_exact(&mut rcv_length_bytes)?;
-        
+
             // convert bytes to big endian (+2 for return code)
             let rcv_length = u32::from_be_bytes(rcv_length_bytes) + 2;
 
@@ -50,7 +51,7 @@ impl TransportTCP {
             stream.read_exact(&mut buf)?;
             Ok(APDUAnswer::from_answer(buf))
         }
-        
+
         wrap_err(&raw_command, &mut stream).map_err(|_| LedgerTCPError::InnerError)
     }
 }
