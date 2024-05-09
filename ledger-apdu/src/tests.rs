f452d292e523125adfda86ca88933d28b5fe0f09
Juan Leni <juan.leni@zondax.ch>
2024-05-09 16:46:04 +0200

    apply cargo fmt

--- ledger-apdu/src/tests.rs
+++ ledger-apdu/src/tests.rs
@@ -23,55 +23,49 @@ const APDU_RESPONSE: &[u8] = &[0xDE, 0xAD, 0xBE, 0xEF, 0x90, 0x00];
 fn apdu_command_vec() {
     let data = std::vec![SERIALIZED_APDU[5]; 3];
 
-    let command = APDUCommand {
-        cla: 0xFF,
-        ins: 0x00,
-        p1: 0,
-        p2: 0,
-        data,
-    };
+    let command = APDUCommand { cla: 0xFF, ins: 0x00, p1: 0, p2: 0, data };
 
     assert_eq!(SERIALIZED_APDU, &command.serialize()[..])
 }
 
 #[test]
 fn apdu_command_slice() {
-    let data = &SERIALIZED_APDU[5..];
+    let data = &SERIALIZED_APDU[5 ..];
 
-    let _ = APDUCommand {
-        cla: 0xFF,
-        ins: 0x00,
-        p1: 0,
-        p2: 0,
-        data,
-    };
+    let _ = APDUCommand { cla: 0xFF, ins: 0x00, p1: 0, p2: 0, data };
 }
 
 #[test]
 fn apdu_answer_success() {
     let answer = APDUAnswer::from_answer(APDU_RESPONSE).expect("valid answer length >= 2");
 
-    let code = answer.error_code().expect("valid error code");
+    let code = answer
+        .error_code()
+        .expect("valid error code");
     assert_eq!(code, APDUErrorCode::NoError);
 
-    assert_eq!(answer.apdu_data(), &APDU_RESPONSE[..4]);
+    assert_eq!(answer.apdu_data(), &APDU_RESPONSE[.. 4]);
 }
 
 #[test]
 fn apdu_answer_vec() {
     let answer = APDUAnswer::from_answer(APDU_RESPONSE.to_vec()).expect("valid answer length >= 2");
 
-    let code = answer.error_code().expect("valid error code");
+    let code = answer
+        .error_code()
+        .expect("valid error code");
     assert_eq!(code, APDUErrorCode::NoError);
 
-    assert_eq!(answer.apdu_data(), &APDU_RESPONSE[..4]);
+    assert_eq!(answer.apdu_data(), &APDU_RESPONSE[.. 4]);
 }
 
 #[test]
 fn apdu_answer_error() {
     let answer = APDUAnswer::from_answer(&[0x64, 0x00][..]).expect("valid answer length >= 2");
 
-    let code = answer.error_code().expect("valid error code");
+    let code = answer
+        .error_code()
+        .expect("valid error code");
     assert_eq!(code, APDUErrorCode::ExecutionError);
 
     assert_eq!(answer.apdu_data(), &[]);
@@ -79,9 +73,11 @@ fn apdu_answer_error() {
 
 #[test]
 fn apdu_answer_unknown() {
-    let answer = APDUAnswer::from_answer(&APDU_RESPONSE[..4]).expect("valid answer length >= 2");
+    let answer = APDUAnswer::from_answer(&APDU_RESPONSE[.. 4]).expect("valid answer length >= 2");
 
-    let code = answer.error_code().expect_err("invalid error code");
+    let code = answer
+        .error_code()
+        .expect_err("invalid error code");
     assert_eq!(code, 0xBEEF);
 
     assert_eq!(answer.apdu_data(), &[0xDE, 0xAD]);
